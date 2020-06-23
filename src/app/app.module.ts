1bbeaf8b7592438503b3d9de8f4a80f0de615878
pranav777 <pranavamlekar@gmail.com>
2020-06-23 17:03:55 +0530

    Code pushed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExcelsheetComponent } from './excelsheet/excelsheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExcelsheetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
